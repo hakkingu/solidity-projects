56c9e6111809ad43469b852eff9bfc617fc7fa1d
ポルト <118148112+hakkingu@users.noreply.github.com>
2023-01-28 18:22:06 -0300

    Create OpenAfter.sol

---
+++ contracts/OpenAfter.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.16;
+
+contract OpenAfter {
+    mapping(uint256 => Person) public people;
+    uint256 peoplecount = 0;
+    uint256 timer = 1674940706;
+
+    modifier onlyIfItsOpen() {
+        require(block.timestamp >= timer);
+        _;
+    }
+
+  
+    struct Person {
+        uint256 peoplecount;
+        string name;
+        uint256 age;
+    }
+
+    function addPerson(string memory name, uint256 age) public onlyIfItsOpen {
+        increment();
+        people[peoplecount] = Person(peoplecount, name, age);
+    }
+
+    function increment() internal {
+        peoplecount += 1;
+    }
+}
