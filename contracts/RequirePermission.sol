bce4c39a78537ab58666e11a54d296ed48365bef
ポルト <118148112+hakkingu@users.noreply.github.com>
2023-01-28 17:15:43 -0300

    Update and rename requirePermission.sol to RequirePermission.sol

--- contracts/RequirePermission.sol
+++ contracts/RequirePermission.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.16;
 
-contract requirePermission {
+contract RequirePermission {
     mapping(uint256 => Person) public people;
     uint256 peoplecount = 0;
 
